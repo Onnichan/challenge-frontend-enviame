7e357782fbdd5917bc511e252e2b58777235d6e9
Daniel Aguilar <dyangeltk17@gmail.com>
2022-06-14 02:21:30 -0500

    :sparkles: new interface added

--- exercise 2/index.js
+++ exercise 2/index.js
@@ -1,127 +1,66 @@
-let values = {
-  1: {
-    carrier: "CCH",
-    service: "DEX",
-  },
-  2: {
-    carrier: "CCH",
-    service: "express",
-  },
-  3: {
-    carrier: "CCH",
-    service: "priority",
-  },
-  15: {
-    carrier: "CHP",
-    service: "nextday",
-  },
-  16: {
-    carrier: "CHP",
-    service: "sameday",
-  },
-  17: {
-    carrier: "CHP",
-    service: "express",
-  },
-};
+const btn = document.getElementById("btn");
+const code = document.getElementById("code");
+const preValues = document.getElementById('values');
+const preJson = document.getElementById('json');
 
-let json = {
-  data: {
-    BUIN: [
-      {
-        limit: 1,
-        over_carrier_service_id: 17,
-        under_carrier_service_id: 17,
-      },
-      {
-        limit: 2,
-        over_carrier_service_id: 15,
-        under_carrier_service_id: 15,
-      },
-    ],
-    LAJA: [
-      {
-        limit: 2,
-        over_carrier_service_id: 1,
-        under_carrier_service_id: 1,
-      },
-      {
-        limit: 5,
-        over_carrier_service_id: 2,
-        under_carrier_service_id: 2,
-      },
-      {
-        limit: 1,
-        over_carrier_service_id: 17,
-        under_carrier_service_id: 17,
-      },
-    ],
-
-    LEBU: [
-      {
-        limit: 2,
-        over_carrier_service_id: 1,
-        under_carrier_service_id: 1,
-      },
-      {
-        limit: 6,
-        over_carrier_service_id: 3,
-        under_carrier_service_id: 3,
-      },
-      {
-        limit: 5,
-        over_carrier_service_id: 2,
-        under_carrier_service_id: 2,
-      },
-      {
-        limit: 4,
-        over_carrier_service_id: 16,
-        under_carrier_service_id: 16,
-      },
-    ],
-
-    LOTA: [
-      {
-        limit: 2,
-        over_carrier_service_id: 15,
-        under_carrier_service_id: 15,
-      },
-      {
-        limit: 4,
-        over_carrier_service_id: 16,
-        under_carrier_service_id: 16,
-      },
-      {
-        limit: 1,
-        over_carrier_service_id: 17,
-        under_carrier_service_id: 17,
-      },
-    ],
-  },
-};
-
+window.addEventListener('DOMContentLoaded', () => {
+  
+  preValues.innerHTML =  syntaxHighlight(JSON.stringify(values, null, 2));
+  preJson.innerHTML =  syntaxHighlight(JSON.stringify(json, null,2));
+})
+//Function that convert to join both json
 function treatment(firstDoc, secondDoc) {
-  // const result = {... secondDoc.data};
   const result = _.cloneDeep(secondDoc);
-  // console.log(result);
-  // for (obj in secondDoc.data) {
-  //   const red = secondDoc.data[obj].reduce(
-  //     (current, next) => (current.limit > next.limit ? current : next),
-  //     {}
-  //   );
-  //   Object.defineProperties(obj, key)
-  //   red['over'] = red['over_carrier_service_id'];
-  //   red['under'] = red['under_carrier_service_id'];
-  //   secondDoc.data[obj] = red;
-  //   for(key in values){
-  //     if(key == red['carrier']){
-  //       red['carrier'] = values[key];
-  //       red['service'] = values[key];
-  //     }
-  //   }
-  // }
-  // console.log(secondDoc.data);
-  console.log(result);
-  return {};
+  const _values = _.cloneDeep(firstDoc);
+
+  for (key in result.data) {
+    const red = result.data[key].reduce(
+      (current, next) => (current.limit > next.limit ? current : next),
+      {}
+    );
+    red["over"] = red["over_carrier_service_id"];
+    red["under"] = red["under_carrier_service_id"];
+    result.data[key] = red;
+    delete result.data[key]["over_carrier_service_id"];
+    delete result.data[key]["under_carrier_service_id"];
+    for (key in _values) {
+      if (key == red["over"]) {
+        red["over"] = _values[key];
+        red["under"] = _values[key];
+      }
+    }
+  }
+  return result.data;
 }
-treatment(values, json);
+
+console.log(treatment(values, json));
+
+btn.addEventListener("click", () => {
+  // code.innerHTML = JSON.stringify(treatment(values, json), null, 2);
+  code.innerHTML = syntaxHighlight(JSON.stringify(treatment(values, json), null, 2));
+});
+
+function syntaxHighlight(json) {
+  if (typeof json != "string") {
+    json = JSON.stringify(json, undefined, 2);
+  }
+  json = json.replace(/&/g, "&").replace(/</g, "<").replace(/>/g, ">");
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      var cls = "number";
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = "key";
+        } else {
+          cls = "string";
+        }
+      } else if (/true|false/.test(match)) {
+        cls = "boolean";
+      } else if (/null/.test(match)) {
+        cls = "null";
+      }
+      return '<span class="' + cls + '">' + match + "</span>";
+    }
+  );
+}
